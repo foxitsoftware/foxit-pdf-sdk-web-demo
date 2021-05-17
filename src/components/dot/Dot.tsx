3079b1231f37b6e1bab8531114204a408fcd777d
Ігор Шимков <ihossa17@gmail.com>
2021-05-17 11:40:52 +0300

    add pulse dot

---
+++ src/components/dot/Dot.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Dot.css";
+
+export const Dot =  () => {
+    return (
+        <div className="ring-container">
+            <div className="ringring"></div>
+            <div className="circle"></div>
+        </div>
+    );
+  }
