4d3e42911518ba14d892b38d87e757ed0f1f675e
luonan_chen <luonan_chen@foxitsoftware.cn>
2021-12-10 15:40:54 +0800

    fix:WEBPDFRD-5899 allowFullScreen

--- src/App.tsx
+++ src/App.tsx
@@ -251,6 +251,7 @@ const App = () => {
                         ref={iframeRef}
                         className="fv__catalog-app-previewer"
                         src={it.path}
+                        allowFullScreen
                       ></iframe>
                     </Route>
                   );
