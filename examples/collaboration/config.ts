cb9a0529e67a3197f630a34c25fa038076118276
jason_he <jason_he@me.com>
2022-10-24 11:27:04 +0800

    add production deployment config

--- examples/collaboration/config.ts
+++ examples/collaboration/config.ts
@@ -2,8 +2,12 @@ declare var HTTP_BASE_URL: string; // base url of collab server http api, define
 // declare var WS_BASE_URL: string; // base url of collab server websocket api, defined by build tool(webpack)
 declare var __webpack_public_path__: string;
 
-// export const serverUrl = `https://${window.location.host}/collab-server`;
-export const serverUrl = `http://${window.location.hostname}:19112`;
+export let serverUrl = `http://${window.location.hostname}:19112`;
+
+// config for production deployment
+if(window.location.protocol === 'https:'){
+  serverUrl = `https://${window.location.hostname}:19112/collab`;
+}
 
 export const PUBLIC_PATH = __webpack_public_path__;
 
